8f53a822d66ca8a5fab981ec6f984d8705174250
arandomguyhi <theorda52@gmail.com>
2024-09-05 18:46:12 -0300

    geometry dash modchart

--- data/transient-tribulation/modchart.lua
+++ data/transient-tribulation/modchart.lua
@@ -1,8 +1,24 @@
-
+local counter = -1
 function onStepHit()
+    if curStep >= 656 and curStep < 916 then
+	if curStep % 4 == 0 then
+	    counter = counter * -1
+
+	    for i = 0,7 do
+		local name = i > 3 and "defaultPlayerStrum" or "defaultOpponentStrum"
+		local note = (i % 4)
+
+		if i == 0 or i == 2 or i == 4 or i == 6 then noteTweenY('wewekjkv'..i, i, _G[name..'Y'..note] - 15 * counter, (stepCrochet/1000), 'quadInOut')end
+		if i == 1 or i == 3 or i == 5 or i == 7 then noteTweenY('wewekjre'..i, i, _G[name..'Y'..note] + 15 * counter, (stepCrochet/1000), 'quadInOut')end
+	    end
+	end
+    end
+
     if curStep == 916 then
-	for i = 0,3 do
-	    noteTweenY('wewekjkb'..i, i, _G['defaultOpponentStrumX'..i], (stepCrochet/1000), 'quadInOut')
+	for i = 0,7 do
+	    local name = i > 3 and "defaultPlayerStrum" or "defaultOpponentStrum"
+	    local note = (i % 4)
+	    noteTweenY('defaultY'..i, i, _G[name..'Y'..note], (stepCrochet/1000), 'quadInOut')
 	end
     end
 end
